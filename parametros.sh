de7c4bf11b1b8ec067122fa6649b23f475ff6435
loki0b <riqc0nt4ct@hotmail.com>
2023-03-01 22:07:40 -0300

    add aula parametros

---
+++ parametros.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+echo "Parametro 1: $1"
+echo "Parametro 2: $2"
+
+echo "Todos os parametros: $*"
+
+echo "Quantidade de parametros: $#"
+
+echo "Saida do ultimo parametro: $?"
+
+echo "Process ID: $$"
+
+echo "Nome do script: $0"
